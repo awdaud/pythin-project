34a0243b95c573cbaf7eaa07142162084b6417be
awdaud <wahab@sysloggh.com>
2021-11-01 18:18:42 +0000

    my first python project

--- lab.py
+++ lab.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
     
 if __name__ == "__main__":
     app.run(debug=True)
